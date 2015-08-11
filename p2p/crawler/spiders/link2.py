4d478152130597c8a154fe640d70a7bafdee595a
william.wu <53772980@qq.com>
2015-08-11 22:49:02 +0800

    enabled redis spider

--- p2p/crawler/spiders/link2.py
+++ p2p/crawler/spiders/link2.py
@@ -5,9 +5,9 @@ import redis_spider
 class Link2Spider(redis_spider.RedisSpider):
     name = "link2"
     allowed_domains = ["www.baidu.com"]
-    start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-    "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"]
+    #start_urls = [
+     #   "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+    #"http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"]
 
     def parse(self, response):
         print response
